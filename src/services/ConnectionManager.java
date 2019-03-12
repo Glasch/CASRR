93f664a62fb4cfe767e5e5f66115e9604b2ec056
glasch <a.glasch@hotmail.com>
2019-03-12 15:55:14 +0400

    Timestamps Compare

--- src/services/ConnectionManager.java
+++ src/services/ConnectionManager.java
@@ -3,10 +3,17 @@ package services;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.sql.Connection;
@@ -28,6 +35,7 @@ public class ConnectionManager {
         HttpClient client = builder.build();
 
         HttpGet request = new HttpGet(requestString);
+
         request.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36" +
                 " (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
         HttpResponse response;
@@ -50,7 +58,37 @@ public class ConnectionManager {
         return jsonObject;
     }
 
-   public static Connection getDBconnection(String url, String login, String password) throws SQLException, ClassNotFoundException {
+    public static JSONArray getHitBtcBalanceJsonArray(String requestString, String login, String password) throws AuthenticationException {
+
+        Logger.getLogger("org.apache").setLevel(Level.OFF);
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials(login, password));
+        CloseableHttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(credentialsProvider).build();
+        HttpGet request = new HttpGet(requestString);
+        request.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36" +
+                " (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
+        HttpResponse response;
+        JSONArray jsonObject;
+        try {
+            response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+            jsonObject = new JSONArray(EntityUtils.toString(entity));
+            StatusLine statusLine = response.getStatusLine();
+            if (statusLine.getStatusCode() != 200) {
+                System.out.println("Bad response: " + statusLine.getStatusCode() + " " + statusLine.getReasonPhrase());
+                System.out.println("Request: " + requestString);
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Could not get response: " + e.getMessage());
+            System.out.println("Request: " + requestString);
+            return null;
+        }
+        return jsonObject;
+    }
+
+
+    public static Connection getDBconnection(String url, String login, String password) throws SQLException, ClassNotFoundException {
         //Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(url, login, password);
     }
